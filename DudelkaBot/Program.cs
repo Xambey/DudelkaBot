d2672d2d1768fc9b4726d7c67d72ce47286c0761
Xambey <Xambey@yandex.ru>
2017-01-22 01:22:29 +0300

    work sending and receiving messagesэ

---
+++ DudelkaBot/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IRCClient;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace DudelkaBot
+{ 
+
+    class Program
+    {
+        #region Variables
+        static string userName = "DudelkaBot";
+        static string password = "oauth:x2ha2yusryy5dir8xlhkg90rqfpkld";
+        static Thread chatThread;
+        static IrcClient ircClient = new IrcClient("irc.chat.twitch.tv", 6667, userName, password);
+        static NetworkStream serverStream = ircClient.tcpClient.GetStream();
+        static string readData = "";
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            int buffsize = 0;
+
+            byte[] buff = new byte[10025];
+            buffsize = ircClient.tcpClient.ReceiveBufferSize;
+
+            ircClient.joinRoom("xiixalex");
+            chatThread = new Thread(new ThreadStart(Process));
+            chatThread.Start();
+        }
+
+        private static void Process()
+        {
+            while (true)
+            {
+                try
+                {
+                    readData = ircClient.readMessage();
+                    int pos = readData.LastIndexOf(':');
+                    readData = new string(readData.Skip(pos + 1).ToArray());
+
+                    switch (readData)
+                    {
+                        case "!hi":
+                            ircClient.sendChatMessage("ДуделкаБот работает!");
+                            break;
+                        case "!date":
+                            ircClient.sendChatMessage(DateTime.Now.ToString());
+                            break;
+                        case "!просыпайся тварь":
+                            ircClient.sendChatMessage("Да, хозяин!");
+                            break;
+                        case "!умри":
+                            ircClient.sendChatMessage("умываю руки");
+                            ircClient.tcpClient.Close();
+                            return;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ IRCClient/IrcClient.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+
+namespace IRCClient
+{
+    public class IrcClient
+    {
+        public TcpClient tcpClient;
+        private StreamWriter outputStream;
+        private StreamReader inputStream;
+        private int port;
+        private string ipHost, userName, password, channel; 
+
+        public IrcClient(string ipHost, int port, string userName, string password)
+        {
+            try {
+                this.ipHost = ipHost;
+                this.port = port;
+                this.userName = userName;
+                this.password = password;
+
+                tcpClient = new TcpClient(ipHost, port);
+                inputStream = new StreamReader(tcpClient.GetStream());
+                outputStream = new StreamWriter(tcpClient.GetStream());
+
+                Console.OutputEncoding = Encoding.Default;
+
+                signIn();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+
+        private void signIn()
+        {
+            if (outputStream == null)
+            {
+                throw new Exception("Error of sign in\n");
+            }
+
+            outputStream.WriteLine("PASS " + password);
+            outputStream.WriteLine("NICK " + userName);
+            outputStream.WriteLine("USER" + userName);
+            outputStream.WriteLine("CAP REQ :twitch.tv/membership");
+            outputStream.WriteLine("CAP REQ :twitch.tv/commands");
+            outputStream.Flush();
+        }
+
+        public void joinRoom(string channel)
+        {
+            try {
+                this.channel = channel;
+                outputStream.WriteLine("JOIN #" + channel);
+                outputStream.Flush();
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Error, client is shutdown...\n" +  ex.Message);
+                return;
+            }
+        }
+
+        public void leaveRoom()
+        {
+            if(outputStream != null)
+                outputStream.Close();
+            if (inputStream != null)
+                inputStream.Close();
+        }
+
+        private void sendIrcMessage(string message)
+        {
+            try {
+                if (outputStream == null)
+                    throw new Exception("Output stream is empty...");
+                outputStream.WriteLine(message);
+                outputStream.Flush();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+
+        public void sendChatMessage(string message)
+        {
+            sendIrcMessage(":" + userName + "!" + userName + "@" + userName + "twi.twitch.tv PRIVMSG #" + channel + " :" + message);
+        }
+
+        public void pingResponse()
+        {
+            sendIrcMessage("PONG twi.twitch.tv\r\n");
+        }
+
+        public string readMessage()
+        {
+            return inputStream.ReadLine();
+        }
+    }
+}
